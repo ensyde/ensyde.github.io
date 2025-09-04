a664fffeda27379fb3804a3e402d87e1a7fcc3c3
ensyde <ensyde0@gmail.com>
2025-09-04 07:35:41 -0400

    initial

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/ensyde.github.io",
+  images: {
+    unoptimized: true,
+  },
+  /* config options here */
+};
+
+export default nextConfig;
